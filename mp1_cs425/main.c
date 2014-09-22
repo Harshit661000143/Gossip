26a978f266c1ec964dba2d631401d8b27fc46582
Harshit <hdokani2@illinois.edu>
2014-09-21 19:59:53 -0500

    Initial Commit

---
+++ mp1_cs425/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  mp1_cs425
+//
+//  Created by HARSHIT DOKANIA on 9/21/14.
+//  Copyright (c) 2014 HARSHIT DOKANIA. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
